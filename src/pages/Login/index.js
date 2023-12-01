c064f784c33d7deb35c8a88186f4e8736e7ee6c4
Gabriel Abal <gabriel.maia.abal@gmail.com>
2023-12-01 16:59:50 -0300

    feat: criado o componente LogoMarketCubos

---
+++ src/pages/Login/index.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+import LogoMarketCubos from "../../components/LogoMarketCubos";
+
+function Login() {
+    return (
+        <div>
+            <LogoMarketCubos />
+        </div>
+    )
+}
+
+
+export default Login;
