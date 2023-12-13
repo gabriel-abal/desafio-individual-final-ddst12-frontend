912613fb6ef7000daa84e4ea8fcc2c1ab2f90489
Gabriel Abal <gabriel.maia.abal@gmail.com>
2023-12-13 09:31:22 -0300

    feat: importação da página cadastro

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Login from '../src/pages/Login/index'
+import Cadastro from './pages/Cadastro';
 
 function App() {
   return (
     <Login />
+    // <Cadastro />
   );
 }
 
