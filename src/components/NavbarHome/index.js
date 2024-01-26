166d6360c5eca0828f96380e174ff26ffe47f13c
Gabriel Abal <gabriel.maia.abal@gmail.com>
2024-01-26 09:46:52 -0300

    feat: criado o componente NavbarHome

---
+++ src/components/NavbarHome/index.js
@@ -0,0 +1,18 @@
+import './styles.css'
+import React from 'react'
+import LogoMarketCubosMain from '../LogoMarketCubosMain/index'
+
+const NavbarHome = () => {
+    return (
+        <nav>
+            <div className='logoContainer'>
+                <LogoMarketCubosMain />
+            </div>
+            <div className='menuContainer'>
+
+            </div>
+        </nav>
+    )
+}
+
+export default NavbarHome
